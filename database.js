e94899c3ba06622d0981f6738ed06cf5c9934f69
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-02-05 14:58:34 +0200

    fix issue #1

--- database.js
+++ database.js
@@ -1,16 +1,9 @@
 import { query } from "./fakeDB";
 
-// Отримати машину за ID
-function getCarById(id) {
-  return query("SELECT * FROM cars WHERE id = $1", [id]);
+function getById(table, id) {
+  return db.query(`SELECT * FROM ${table} WHERE id = $1`, [id]);
 }
 
-// Отримати всі машини
-function getAllCars() {
-  return query("SELECT * FROM cars");
-}
-
-// Додати машину
 function addCar(make, model, year) {
   return query(
     "INSERT INTO cars (make, model, year) VALUES ($1, $2, $3)",
@@ -18,14 +11,8 @@ function addCar(make, model, year) {
   );
 }
 
-// Видалити машину
 function deleteCar(id) {
   return query("DELETE FROM cars WHERE id = $1", [id]);
 }
 
-// Отримати користувача за ID
-function getUserById(id) {
-  return query("SELECT * FROM users WHERE id = $1", [id]);
-}
-
-export default { getCarById, getAllCars, addCar, deleteCar, getUserById };
+export default { getById, addCar, deleteCar };
