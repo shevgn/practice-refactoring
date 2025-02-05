4555535234396a55c10ec6a7cf3486a250e203ac
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-02-05 14:53:24 +0200

    magic number example

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import carRoutes from "./backend/carsRoutes";
 app.use(json());
 app.use("/api/cars", carRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
